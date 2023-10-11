9f9fe2f2653dd55a9865f02ad9b5f667d80044d2
tharun <madoththarun22@gamail.com>
2023-10-11 14:40:13 +0000

    solution

---
+++ src/components/DenominationItem/index.js
@@ -0,0 +1,24 @@
+// Write your code here
+import './index.css'
+
+const DenominationItem = props => {
+  const {item, denominatorValue} = props
+  const {value} = item
+
+  const onDenominatorValue = () => {
+    denominatorValue(value)
+  }
+  return (
+    <li className="denomination-item">
+      <button
+        type="button"
+        className="denomination-button"
+        onClick={onDenominatorValue}
+      >
+        {value}
+      </button>
+    </li>
+  )
+}
+
+export default DenominationItem
